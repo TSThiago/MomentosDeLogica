bf9ffb444035c76dc3ece29329207b939574c02c
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-11-16 19:33:54 -0300

    percentual error fixed

--- algoritmo.js
+++ algoritmo.js
@@ -1,9 +1,9 @@
 let valorNegativo = false
-let salarioMaior = 0
-let contadorEntrevistados = 0
-let mediaSalario = 0
-let mediaFilhos = 0
-let percentual = 0
+let salarioMaior = parseFloat(0)
+let contadorEntrevistados = parseInt(0)
+let mediaSalario = parseFloat(0)
+let mediaFilhos = parseFloat(0)
+let percentual = parseInt(0)
 
 while(valorNegativo === false){
     let salario = parseFloat(prompt("Qual o seu salário"))
@@ -32,10 +32,10 @@ while(valorNegativo === false){
 
 mediaSalario = mediaSalario/contadorEntrevistados
 mediaFilhos = mediaFilhos/contadorEntrevistados
-percentual = percentual/contadorEntrevistados
+percentual = (percentual/contadorEntrevistados) * 100
 
 console.log("Média Salarial: " +(mediaSalario))
 console.log("Média Filhos: " +(mediaFilhos))
 console.log("Maior Salário: " +salarioMaior)
-console.log("Percentual de entrevistados com salário menos que R$150,00: " +percentual)
+console.log("Percentual de entrevistados com salário menos que R$150,00: " +percentual+ "%")
 console.log("Número de entrevistados: " +contadorEntrevistados)
